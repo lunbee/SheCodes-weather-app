4ab1e89e5a04ed7799806202edfa9a2842abc1dd
luna-beeker <82769715+lunbee@users.noreply.github.com>
2021-04-30 12:09:44 +0200

    Added relevant icon from API and fixed some bugs

--- src/app.js
+++ src/app.js
@@ -1,10 +1,39 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp)
+    let hours = date.getHours()
+    if (hours < 10){
+        hours = `0${hours}`
+    }
+
+    let minutes = date.getMinutes()
+    if (minutes < 10){
+       minutes = `0${minutes}`
+    }
+
+
+    let days = [
+        "Sunday", 
+        "Monday", 
+        "Tuesday", 
+        "Wednesday", 
+        "Thursday", 
+        "Friday", 
+        "Saturday"
+        ]
+        
+    let day = days[date.getDay()]
+    
+    return `${days[day]} ${hours}:${minutes}`
+}
+
+
 function displayTemperature(response) {
-   console.log(response.data)
+
 
     let temperatureElement = document.querySelector("#temperature")
     temperatureElement.innerHTML = Math.round (response.data.main.temp)
 
-    let cityElement = document.querySelector("#city-input")
+    let cityElement = document.querySelector("#city")
     cityElement.innerHTML = response.data.name
 
     let weatherDescription = document.querySelector("#weather-description")
@@ -14,13 +43,21 @@ function displayTemperature(response) {
     humidity.innerHTML = response.data.main.humidity
 
     let windSpeed = document.querySelector("#wind-speed")
-    windSpeed.innerHTML = response.data.wind.speed
+    windSpeed.innerHTML = Math.round (response.data.wind.speed)
+
+     let dateElement = document.querySelector("#date")
+    dateElement.innerHTML = formatDate(response.data.dt * 1000)
+
+    let iconElement = document.querySelector("#icon")
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` ) 
+
+    iconElement.setAttribute("alt", response.data.weather[0].description )
 }
 
 let apiKey = "adc68ed824eaa0c6b8e179f70c0efacc"
-let city = "amsterdam"
+let city = "Bangkok"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-console.log(apiUrl)
+
 
 axios.get(apiUrl).then(displayTemperature);
 
