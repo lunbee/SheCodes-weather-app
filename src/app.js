d42c74227d433386f91493a3b0f6512f6f0960f6
luna-beeker <82769715+lunbee@users.noreply.github.com>
2021-05-13 09:45:29 +0200

    Display the forecast data using the API

--- src/app.js
+++ src/app.js
@@ -26,30 +26,48 @@ function formatDate(timestamp) {
     return `${days[day]} ${hours}:${minutes}`
 }
 
-function displayForecast() {
+
+function formatDay(timestamp){
+    let date = new Date(timestamp * 1000)
+    let day = date.getDay()
+    let days = [
+        "Sun", 
+        "Mon", 
+        "Tue", 
+        "Wed", 
+        "Thu", 
+        "Fri", 
+        "Sat"
+    ]
+
+    return days[day]
+}
+
+function displayForecast(response) {
+    let forecast = response.data.daily
     let forecastElement = document.querySelector("#forecast")
 
     let forecastHTML = `<div class="row">`
     let days = ["Thu", "Fri", "Sat", "Sun"]
-    days.forEach(function(day) {
-
+    forecast.forEach(function(forecastDay, index) {
+    if (index < 6) {
     forecastHTML = 
         forecastHTML + 
 
            `
             <div class="col-2">
-                <div class="weather-forecast-date">
-                ${day}
+                <div class="weather-forecast-date">${formatDay
+                (forecastDay.dt)}
                 </div>
                 <img 
-                src="http://openweathermap.org/img/wn/02d@2x.png" 
+                src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
                 alt="" 
                 width="35"/>
                 <div class="min-max-temperature"> <span class="max-temperature">
-                18° </span><span class="min-temperature">12°</span> 
+                ${Math.round(forecastDay.temp.max)}° </span><span class="min-temperature">${Math.round(forecastDay.temp.min)}°</span> 
             </div>
         </div>`
-
+        }
     })
 
 
@@ -58,6 +76,14 @@ function displayForecast() {
 
 }
 
+
+function getForecast(coordinates){
+    console.log(coordinates)
+    let apiKey = "adc68ed824eaa0c6b8e179f70c0efacc"
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
+    console.log(apiUrl)
+    axios.get(apiUrl).then(displayForecast)
+}
 
 function displayTemperature(response) {
 
@@ -87,11 +113,12 @@ function displayTemperature(response) {
 
     celciusTemperature = response.data.main.temp
 
-
+    getForecast (response.data.coord)
+    
 }
 
 search("New York")
-displayForecast()
+
 
 
 function search(city){
@@ -99,6 +126,7 @@ function search(city){
     let apiKey = "adc68ed824eaa0c6b8e179f70c0efacc"
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
     axios.get(apiUrl).then(displayTemperature);
+
 }
 
 
